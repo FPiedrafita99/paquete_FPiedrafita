72d0d35ab2e7f05b324f7c3ec86fde1840a3165b
FPiedrafita99 <759415@unizar.es>
2026-02-25 20:52:53 +0100

    creacion script plantilla para funcion

---
+++ R/script.R
@@ -0,0 +1,19 @@
+#' Placeholder
+#'
+#' \code{nombre-funcion} placeholder
+#'
+#' @param x un objeto con valores numéricos
+#' @param y un objeto con valores numéricos de la misma clase que x
+#'
+#' @return valores numéricos del índice
+#'
+#'
+#' @examples
+#'
+#'
+#'
+#'
+#' @export
+
+
+
