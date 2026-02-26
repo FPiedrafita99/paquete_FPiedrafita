411ad2f95b7945719d740a628f39c85c55ef2fc2
FPiedrafita99 <759415@unizar.es>
2026-02-26 19:53:00 +0100

    funcion ampliada a m2 corregida y añadidos ejemplos

--- R/script.R
+++ R/script.R
@@ -3,20 +3,29 @@
 #' \code{calArea} calcula el area de un poligono en metros cuadrados o hectáreas
 #'
 #' @param poligono un objeto espacial sf
-#' @param unidad unidad de medida en la que se quiera obtener el area
+#' @param unidad unidad de medida de superficie en la que se quiera obtener el area (ha, m2)
 #'
 #' @return El area del polígono en hectáreas
 #' @importFrom sf st_area
 #'
 #' @examples
 #' poligono <- st_polygon(list(cbind(c(6, 10, 10, 6, 6), c(6, 6, 10, 10, 6))))
-#' calHA(poligono)
+#' CalArea(poligono, 'ha')
+#' poligono <- st_polygon(list(cbind(c(6, 10, 10, 6, 6), c(6, 6, 10, 10, 6))))
+#' calArea(poligono, 'm2')
+#'
 #' @export
 
 calArea <- function(poligono, unidad){
+
   area_m2 <- sf::st_area(poligono)
-  hectareas <- as.numeric(area_m2 / 10000)
-  return(hectareas)
+  if (unidad == 'ha'){
+  resultado <- as.numeric(area_m2 / 10000)
+  return(resultado)
+  } else if (unidad == 'm2'){
+    return (as.numeric(area_m2))
+
+  }
 }
 
 
